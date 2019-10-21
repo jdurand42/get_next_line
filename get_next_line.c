1df2f0201727633b840f5068be0dcc9ef5399b68
Jerome DURAND <jdurand@e1r5p7.42.fr>
2019-10-21 15:01:10 +0200

    it just works - ted howard

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,7 @@
 /*   By: jdurand <jdurand@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/16 13:25:50 by jdurand           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2019/10/21 13:31:49 by jdurand          ###   ########.fr       */
-=======
-/*   Updated: 2019/10/21 14:53:11 by jdurand          ###   ########.fr       */
->>>>>>> b4
+/*   Updated: 2019/10/21 15:00:30 by jdurand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,33 +14,6 @@
 
 int	get_next_line(int fd, char **line)
 {
-<<<<<<< HEAD
-	char		*s;
-	int 		ret;
-	size_t 		i;
-	char 		*bufer_el;
-	char 		*line_el;
-	//char		*line_buffer;
-	static char		buffer[BUFFER_SIZE + 1];
-
-	*line = ft_strdup(ft_strchr(buffer, '\n'));
-//	printf("line in begloop: %s\n", *line);
-		while ((ret = read(fd, buffer, BUFFER_SIZE)) > 0)
-		{
-			buffer[ret] = 0;
-			i = 0;
-	//		printf("ret: %d, buffer in beg_read_loop: %s\n", ret, buffer);
-			while (buffer[i] != 0 && buffer[i] != '\n')
-				i++;
-			if (buffer[i] == '\n') // We got a line;
-			{
-				if(!(*line = ft_strnjoin(*line, buffer, i)))
-					return (-1);
-				return (1);
-			}
-			*line = ft_strjoin(*line, buffer); // ca marche si BUFFER_SIZE < nb char de line
-//			printf("line in endloop: %s\n", *line);
-=======
 	static char	buffer[BUFFER_SIZE + 1];
 	char		*b_line;
 	char		*buffer_el;
@@ -70,7 +39,6 @@ int	get_next_line(int fd, char **line)
 				return (-1);
 			ft_strcpy(buffer, buffer_el + 1);
 			return (1);
->>>>>>> b4
 		}
 		if (!(b_line = ft_strjoin(b_line, buffer)))
 			return (-1);
